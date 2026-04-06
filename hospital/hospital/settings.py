7897e89f973724cd443edca083da3e4ffe636866
Umar Farooq <umarfarook406@gmail.com>
2026-04-06 14:20:39 +0530

    DB support have been added for the instance

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': os.getenv('DB_HOST'),  # MUST be 'db'
         'PORT': os.getenv('DB_PORT'),
     }
 }
